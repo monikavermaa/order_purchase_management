cc55c025822adcd0a6dd3181ae7bce1fc81d3185
monika <monikaverma@example.com>
2025-02-27 15:59:37 +0530

    dashboard changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -13,7 +13,7 @@ class DashboardController < ApplicationController
     @total_stock_east = warehouse_stock['EAST'] || 0
     @total_stock_west = warehouse_stock['WEST'] || 0
     @stock_status = SoukoZaiko.select(:sku_code).group(:sku_code).sum(:stock)
-    @prefecture_codes = PrefectureCode.select(:code, :name)
-    @recent_items = SoukoZaiko.order(created_at: :desc).limit(5).select(:id, :sku_code, :stock)
+    @prefecture_codes = PrefectureCode.select(:code, :name, :ew_flag)
+    @recent_items = SoukoZaiko.order(created_at: :desc).limit(5).select(:id, :sku_code, :warehouse_code, :stock)
   end
 end
